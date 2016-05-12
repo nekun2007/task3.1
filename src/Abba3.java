b72c5e6cf9da63ea0ccbd58d2a536d860c7d0eb2
Сергей А. Акимов <akimov.s.a@rd.mirea.ru>
2016-05-12 12:12:03 +0300

    Initial commit

---
+++ src/Abba3.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Serge on 12.05.2016.
+ */
+public class Abba3 {
+    public static void main(String[] args){
+        for (int i=1; i<=100; i++){
+            if (i%5==0&&i%7==0){
+                System.out.println("ABBA");
+            }
+            else if (i%5==0){
+                System.out.println("A");
+            }
+            else if (i%7==0){
+                System.out.println("B");
+            }
+            else System.out.println(i);
+        }
+    }
+}
